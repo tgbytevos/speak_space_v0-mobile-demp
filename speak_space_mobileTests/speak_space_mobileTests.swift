8ac375d2dd7236870aa301d74bfb86e725d38e6d
tgbytevos <johnpark969627@gmail.com>
2026-07-30 15:18:55 +0100

    Add iOS app interface and Xcode project

---
+++ speak_space_mobileTests/speak_space_mobileTests.swift
@@ -0,0 +1,18 @@
+//
+//  speak_space_mobileTests.swift
+//  speak_space_mobileTests
+//
+//  Created by tom on 30/07/2026.
+//
+
+import Testing
+
+struct speak_space_mobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
